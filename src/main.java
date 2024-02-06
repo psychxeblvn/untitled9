f09e8ec06396ced8cc840fe805921bc12249a2c8
psychxeblvn <jojoykin@gmail.com>
2024-02-06 19:56:49 +0400

    my first commit2

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+
+        int a = 5;
+        if (a > 0) {
+            System.out.println ("а больше 5");
+        }
+        System.out.println("Конец программы");
+    }
+}
