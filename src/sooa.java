0865743de88fbb710954d3cc42430035b49bf74d
psychxeblvn <jojoykin@gmail.com>
2024-02-06 19:23:32 +0400

    my first commit

---
+++ src/sooa.java
@@ -0,0 +1,19 @@
+public class sooa {
+    public static void main(String[] args) {
+        int a = 2;
+        switch (a){
+            case 0:
+                System.out.println("а равно 0");
+                break;
+            case 1:
+                System.out.println("а равно 1");
+                break;
+            case 2:
+                System.out.println("а равно 2");
+                break;
+            default:
+                System.out.println();
+
+        }
+    }
+}
